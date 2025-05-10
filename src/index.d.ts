f43c67cfd6307d2e07e612b7f4bdacb22df47e50
Kong Chenhui <732792684@qq.com>
2025-05-11 00:48:34 +0800

    feat: 支持配置时钟位置

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,3 +2,8 @@ declare module "flipclock" {
   const FlipClock: any;
   export default FlipClock;
 }
+declare interface Window {
+  wallpaperPropertyListener?: {
+    applyUserProperties: (properties: Record<string, any>) => void;
+  };
+}
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,33 @@ new FlipClock(document.querySelector(".clock"), {
   face: "TwentyFourHourClock",
 });
 
+updateImage();
+
+setInterval(() => {
+  updateImage();
+}, 10 * 60 * 1000);
+
+const position = {
+  x: 50,
+  y: 40,
+  scale: 1,
+};
+
+window.wallpaperPropertyListener = {
+  applyUserProperties: function (properties) {
+    if (properties.position_x) {
+      position.x = properties.position_x.value;
+    }
+    if (properties.position_y) {
+      position.y = properties.position_y.value;
+    }
+    if (properties.scale) {
+      position.scale = properties.scale.value;
+    }
+    updateClockPosition();
+  },
+};
+
 function updateImage() {
   getBingPaperData().then((images) => {
     const img = document.querySelector(".paper") as HTMLImageElement;
@@ -15,8 +42,10 @@ function updateImage() {
   });
 }
 
-updateImage();
-
-setInterval(() => {
-  updateImage();
-}, 10 * 60 * 1000);
+function updateClockPosition() {
+  const clock = document.querySelector(".clock") as HTMLElement;
+  clock.style.left = `${position.x}%`;
+  clock.style.top = `${position.y}%`;
+  clock.style.scale = `${position.scale}`;
+  clock.style.transformOrigin = `left top`;
+}
